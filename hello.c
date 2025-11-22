0f176624966b6760290dafb07f2ef52780f6826b
Christopher Smith <sac1cps@sbcglobal.net>
2025-11-22 14:12:34 -0800

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, world!\n");
+    int a = 1;
+    int b = 2;
+    if (b>a)
+    {
+        printf("b is greater than a");
+    }
+    else
+    {
+        printf("a is greater than b");
+    }
+    
+    return 0;
+}
+
